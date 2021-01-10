640ecaf28382f2777cd0bbd22e0af04b9f212e4a
Lucas da Silva <lucas.dasilva@cubiks.com>
2021-01-10 15:50:03 +0000

    Automatic check the solution when all items have value
    Added option to select items by value on click

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Item } from '../item';
 
 @Component({
@@ -6,24 +6,43 @@ import { Item } from '../item';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
-export class PlayerComponent implements OnInit{
-  @Input()
-  grid: Item[][];
+export class PlayerComponent {
+  @Input() grid: Item[][];
+  @Output() checkGame = new EventEmitter<void>();
 
-  constructor() { }
-
-  ngOnInit(){
-    console.log(this.grid);
-  }
-
-  validateValue(item: Item) {
+  validateItem(item: Item) {
     if (item.value != '' && (isNaN(item.value) || item.value < 1 || item.value > 9)) {
       item.value = '';
+      return;
     }
     item.value = +item.value;
 
     if (item.class === 'wrong'){
       item.class = null;
     }
+
+    if(this.isCompleted()){
+      this.checkGame.emit();
+    }
+  }
+
+  selectItem(selectedItem: Item) {   
+    if (selectedItem.value !== '') {
+      this.grid.map(row => row.map(item => {
+        if (item.value == selectedItem.value) {
+          item.class = 'selected';
+        }
+        else {
+          item.class = null
+        }
+      }));
+    }
+    else {
+      this.grid.map(row => row.map(item => item.class = null));
+    }
+  }
+
+  private isCompleted() {
+    return this.grid.every(row => row.every(item => item.value !== ''));
   }
 }
